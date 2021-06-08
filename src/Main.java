e4bf6dab5d93ecce055af609f1b033d8d10f10c2
muhammadriskyperdana <ikibos13@gmail.com>
2021-06-08 13:15:34 +0800

    Membuat Main Class

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Muhammad";
+        String namaTengah = "Risky";
+        String namaBelakang = "Perdana";
+
+        int usia = 19;
+        int targetTahunKuliah = 4;
+        double ipk = 3.8889;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan+namaTengah+namaBelakang );
+    }
+}
