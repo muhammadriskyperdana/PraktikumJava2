dff6ab015c46f66062b6f1e076fcd123a29217e2
muhammadriskyperdana <ikibos13@gmail.com>
2021-06-15 11:56:49 +0800

    Format tanggal dan try catch

--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -1,18 +1,29 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MahasiswaBeraksi {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+
+        String polaTanggal = "dd-MM-yyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(polaTanggal);
+
         //instance - pembuatan object
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "19630366";
         mahasiswa.nama = "Awal";
-        mahasiswa.tanggallahir = new Date();
+        try {
+            mahasiswa.tanggallahir = sdf.parse("02-01-2000");
 
-        System.out.println("NPM \t\t\t : " + mahasiswa.npm);
-        System.out.println("Nama \t\t\t :" + mahasiswa.nama);
-        System.out.println("Tanggal Lahir \t :" + mahasiswa.tanggallahir);
+            System.out.println("NPM \t\t\t : " + mahasiswa.npm);
+            System.out.println("Nama \t\t\t :" + mahasiswa.nama);
+            System.out.println("Tanggal Lahir \t :" + sdf.format(mahasiswa.tanggallahir));
+        } catch (ParseException e) {
+            System.err.println("Format tanggal tidak sesuai");
+        }
+
 
 
     }
